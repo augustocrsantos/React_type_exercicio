d7e2013d8826374ce76002d94a0f6abaaea99020
augusto.santos013 <augusto.santos013@hotmail.com>
2022-10-25 21:40:52 -0300

    api cep

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import axios from "axios";
+import interfaceCep from './InterfaceCep';
 
-function App() {
+
+ function App() {
+  let Icep: interfaceCep = {}
+  let mensagemErro =  ''
+  const[valorcep, setcep] = useState(Icep);
+  const[erro , seterro] = useState(mensagemErro);
+
+ // eslint-disable-next-line 
+ const urlParams = new URLSearchParams(window.location.href);
+
+const url:string = window.location.href;
+let cep:string =  url.split('cep=')[1];
+
+
+if(cep != null || cep != ""){
+let buscaCep:string = 'viacep.com.br/ws/01001000/json/'
+
+const teste =
+axios.get('https://viacep.com.br/ws/'+cep+'/json/')
+  .then(function (response) {
+    setcep({
+      retornacep : "CEP: "+response.data.cep,
+      bairro: "Bairro: "+response.data.bairro,
+      complemento : "Complemento: "+response.data.complemento,
+      localidade : "Localidade: "+response.data.localidade,
+      logradouro : "Logradouro: "+response.data.logradouro,
+      uf : "UF: "+response.data.localidade,
+    })
+     
+    //console.log(response.data.cep);
+  })
+  .catch(function (error) {
+    // handle error
+    seterro(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+}
+
+console.log(url.split('cep=')[1])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
+      <h2>{erro}</h2>
+      <p>{valorcep.retornacep}</p>      
+      <p>{valorcep.complemento}</p>     
+      <p>{valorcep.localidade}</p>     
+      <p>{valorcep.logradouro}</p>
+      <p>{valorcep.uf}</p><br></br>
     </div>
   );
 }
---
+++ src/InterfaceCep.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default interface interfaceCep{
+  retornacep?:string
+  logradouro?:string;
+  complemento?:string;
+  bairro?:string;
+  localidade?:string;
+  uf?:string;
+}
